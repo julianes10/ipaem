d6e3222ca7e89a0408af3d77e1b3ce5e73d2c337
julianes10 <julianes10@yahoo.com>
2017-10-25 23:54:28 +0200

    change the name too in the launcher

--- ipaem.sh
+++ ipaem.sh
@@ -3,4 +3,4 @@ python /opt/ipaem/snowboyHotwords/snowboyHotwords.py
 source /home/eespjul/prj/miniIPA/gasdk/env/bin/activate
 source /home/pi/env/bin/activate
 
-python /opt/ipaem/gaIpaDispatcher/hotword.py 
+python /opt/ipaem/gaIpaDispatcher/gaIpaDispatcher.py 
