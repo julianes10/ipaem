b7a7775f6fea8310f540843aa2f31ce5e283d4b2
julianes10 <julianes10@yahoo.com>
2017-10-28 01:33:16 +0200

    Boot improvement

--- ipaem.sh
+++ ipaem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python /opt/ipaem/snowboyHotwords/snowboyHotwords.py 
+PYTHONPATH=/home/pi/snowboy python /opt/ipaem/snowboyHotwords/snowboyHotwords.py /opt/ipaem/snowboyHotwords/churri.pmdl   /opt/ipaem/snowboyHotwords/chencho.pmdl &
 source /home/eespjul/prj/miniIPA/gasdk/env/bin/activate
 source /home/pi/env/bin/activate
 
