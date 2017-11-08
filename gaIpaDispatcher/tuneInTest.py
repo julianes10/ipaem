31d111bc4953e981590a01867b2fd190c4fa31a1
julianes10 <julianes10@yahoo.com>
2017-11-08 20:24:51 +0100

    wait until ga back

---
+++ gaIpaDispatcher/tuneInTest.py
@@ -0,0 +1,11 @@
+import tuneinWrapper
+from tuneinWrapper import *
+def main():
+
+    test=TuneInRadio()
+    print("Hello")
+    test.run("BBC")
+    test.run("COPE")
+
+if __name__ == '__main__':
+    main()
