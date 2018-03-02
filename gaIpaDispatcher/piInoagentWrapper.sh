5db2f25b7a645c901539128617bee5a2ac24f7d3
julianes10 <julian.espinosa.santos@ericsson.com>
2018-03-02 17:13:04 +0100

    more on lights

--- gaIpaDispatcher/piInoagentWrapper.sh
+++ gaIpaDispatcher/piInoagentWrapper.sh
@@ -8,13 +8,70 @@ function usage
   echo  "$0 
            --color <COLOR>
            --on
+           --mode <fullcolor|rolling|rainbow|noise|knightRider>
            --off   
+           --pause <quicker|slower>
            --help"
   echo "Notes:
            --color will change main color theme on led strip
            <COLOR> should be a color known by python colour module or #RRGGBB html notation  
 "
 }
+
+################################################
+function setMode
+{
+  echo "Setting up specific mode in led strip: \"$1\"..."
+  m='A'
+  c='red'
+  t='9999'
+  p='500'
+  case "$1" in
+        fullColor)
+            m='A'
+            ;;         
+        rolling)
+            m='C'
+            ;;           
+        rainbow)
+            m='W'
+            ;;         
+        noise)
+            m='N'
+            ;;         
+        knightRider)
+            m='K'
+            ;;                  
+        *)
+            echo $"Bad usage"
+            exit 1
+       esac
+
+  curl -i -H "Content-Type: application/json" -X POST -d '{"mode":"'$m'","color":"'$c'","pause":"'$p'","timeout":"'$t'"}' http://localhost:5001/ipaem/api/v1.0/misc
+  echo $t >/tmp/pinoAgent.t
+  echo $p >/tmp/pinoAgent.p
+
+} 
+################################################
+function setPause
+{
+  p=$1
+  p0=`cat /tmp/pinoAgent.p`
+  
+  case "$1" in
+        quicker)
+            p=$(( p + 100 ))
+            ;;         
+        slower)
+            p=$(( p - 100 ))
+            ;;           
+        *)
+            echo $"Try with a number"
+       esac
+  echo $p >/tmp/pinoAgent.p
+  echo "Setting up pause to: \"$p\"..."
+  curl -i -H "Content-Type: application/json" -X POST -d '{"pause":"'$1'"}' http://localhost:5001/ipaem/api/v1.0/pause
+} 
 
 ################################################
 function setColor 
@@ -53,6 +110,12 @@ elif [ "$1" == "--on" ]; then
   exit 0
 elif [ "$1" == "--off" ]; then
   turnOff
+  exit 0
+elif [ "$1" == "--mode" ]; then
+  setMode $2
+  exit 0
+elif [ "$1" == "--pause" ]; then
+  setPause $2
   exit 0
 else
   echo "ERROR, unknown option $1"
---
+++ tellmeGame.md
@@ -0,0 +1,43 @@
+#TELLME GAME
+
+## Brainstorming
+
+### Things that you can do
+
+* Challenge to repeat a sentence. Short and then longer (levels). Speaking and listening
+
+* Repeat after me
+ * Say sentences e.g fortune
+ * From news, headlines or read a news
+ * Say measure units
+
+* Maths and numbers
+ * Do operation and say the result
+ * Having the digits, say full number and viceversa
+
+ 
+* Quiz about¡ 
+ * with an imdb wrapper: main actors, directors, year of a film, tv series...
+ * end a proverb or say the begining
+ * sing a song and check the lyrics
+
+* Translate: $ pip install googletrans
+
+* play with leds: 
+ * say colors
+ * say faster/quicker slower
+ * ask for timer with light
+
+### Feedback
+Emit sounds for ok or ko
+Say random sentences for encourage
+Flash red light, green or happy lightning¡
+
+### Gaming
+Setup the number of players, record their names 
+Creating profiles per users --> data mining¡¡¡¡ remember its credits records, challenging them to improve¡¡¡¡ 
+After saying its name, search for the profile
+
+Select level or take it from profile
+
+
