d6f8d6aaf272ca7e355880450e4bc5377fc84222
julianes10 <julian.espinosa.santos@ericsson.com>
2018-04-05 23:44:34 +0200

    tts rss autodetect language

--- gaIpaDispatcher/rssWrapper.sh
+++ gaIpaDispatcher/rssWrapper.sh
@@ -1,43 +1,92 @@
 #!/bin/bash 
-echo "This act as a wrapper to tts rss news"
+echo "This script is as a wrapper to tts rss news"
 #args: $1(stationId | STOP) 
 echo "Here we go $@"
 
 function usage
 {
-  echo "$0 (rss | STOP)" 
+  echo  "$0 
+           --url  <URLFSS>  [--items <NUM ITEMS TO READ>]   
+           --stop 
+           --help"
+  echo "Notes:
+          --stop only killall tts tool
+          --items default 3"
 }
 
-if [ "$1" == "-h" -o "$1" == "--help" ]; then
-  usage
-  exit 0
-fi
-
-if [ "$1" == "" ]; then
-  echo "ERROR, args needed"
-  usage
-  exit -1
-fi
-
+################################################
+function stop 
+{
 #Stop whatever it is playing
 echo "Stopping rss speech..."
 killall pico2wave
+}
 
-if [ "$1" == "STOP" ]; then
-   exit 0
-fi
+################################################
+function dilo 
+{
+  LAN="es-ES"
+  #LAN="en-GB"
+  pico2wave -l $LAN -w /tmp/lookdave.wav "$1" && aplay /tmp/lookdave.wav
+}
 
-#If args is not just stop, then let's put the rss to sing
+################################################
+function sayIt 
+{
+  LANF=""
+  #LANF="--language spanish"
+  echo "$1" | festival --tts  $LANF 
+}
+
+################################################
+function readRSSURL 
+{
 echo "Checkout rss news from: $1..."
 
-feed -o $1 | head -n3 >/tmp/news
+curl $1 --output /tmp/news.raw
+feed -o /tmp/news.raw | head -n$2 >/tmp/news
+
+grep "<language>" /tmp/news.raw | grep "es"
+spanish="$?"
+
+LAN="en-GB"
+LANF=""
+if [ "$spanish" == "0" ]; then
+  LAN="es-ES"
+  LANF="--language spanish"
+fi
+echo "Language: $LAN ( $spanish )..."
+
 input=/tmp/news
 while IFS= read -r var
 do
-  pico2wave -l es-ES -w /tmp/lookdave.wav "$var" && aplay /tmp/lookdave.wav
+  if [ "$spanish" == "0" ]; then
+    dilo "$var"
+  else
+    sayIt "$var"
+  fi
   sleep 1
 done < "$input"
+} 
+################################################
+################################################
+################################################
 
-
-
+if [ "$1" == "-h" -o "$1" == "--help" ]; then
+  usage
+elif [ "$1" == "--stop" ]; then
+   stop
+elif [ "$1" == "--url" ]; then
+  #If args is not just stop, then let's put the rss to sing
+  items=3
+  if [ "$3" == "--items" ]; then
+    $items=$4
+  fi
+  readRSSURL $2 $items
+else
+  echo "ERROR, unknown option $1"
+  usage
+  exit -1
+fi
+exit 0
 
