ac7f39551c92a6b85a246b7d257b47cd46c8191a
julianes10 <julian.espinosa.santos@ericsson.com>
2018-12-01 01:21:43 +0100

    adding all services needed for ipaem

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 PROJECT_NAME=ipaem
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST=piInoAgent
+SERVICES_LIST=gaIpaDispatcher gpioHotword kodi noip2 piInoAgent sbHotword 
 PI_USER=pi
 PI_IPNAME=pi3
 PI_PORT=22
