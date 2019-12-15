14188bed5adfd4a75f16495036ef556bd60f9b56
julianes10 <julianes10@yahoo.com>
2019-12-15 17:05:45 +0100

    Update SCS of all elementes. gassistant not working yet

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 PROJECT_NAME=ipaem
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST=gaIpaDispatcher gpioHotword kodi noip2 piInoAgent sbHotword 
+SERVICES_LIST="systemStatus gaIpaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT"
 PI_USER=pi
-PI_IPNAME=pi3
+PI_IPNAME=192.168.1.43
 PI_PORT=22
 
 
