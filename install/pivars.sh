51316ed4d46820325f38b7190c11a33059780d7a
julianes10 <julianes10@yahoo.com>
2019-12-25 17:47:59 +0100

    gaipa moves to ipa and can be triggered by rest, not only by ga that is still not working

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 PROJECT_NAME=ipaem
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST="systemStatus gaIpaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT"
+#SERVICES_LIST="systemStatus gaIpaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT"
+SERVICES_LIST="systemStatus ipaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT"
 PI_USER=pi
 PI_IPNAME=192.168.1.43
 PI_PORT=22
