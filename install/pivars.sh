1193c1a509fb531971861edfcbde0c69c7a5a835
julianes10 <julianes10@yahoo.com>
2020-01-11 00:57:07 +0100

    adding dht support, preparing for tts usage

--- install/pivars.sh
+++ install/pivars.sh
@@ -2,7 +2,7 @@
 PROJECT_NAME=ipaem
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
 #SERVICES_LIST="systemStatus gaIpaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT"
-SERVICES_LIST="systemStatus ipaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT"
+SERVICES_LIST="systemStatus ipaDispatcher gpioHotword kodi noip2  piInoAgent sbHotword telegramBOT simpleDHTtracker"
 PI_USER=pi
 PI_IPNAME=192.168.1.43
 PI_PORT=22
